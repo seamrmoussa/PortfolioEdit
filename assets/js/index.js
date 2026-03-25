2383aabefcd43f7b88d4448df4c202d9fd5d4496
Amr Moussa <seamrmoussa@gmail.com>
2026-03-25 14:11:43 +0200

    PortfolioEdit
    PortfolioEdit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+// ^ Write your JavaScript code here
+var allSection = document.querySelectorAll("about");
+var aboutSection = document.getElementById("about");
+var heroLink = document.querySelector("[href='#hero-section']");
+var aboutLink = document.querySelector("[href='#about']");
+var bodySection = document.querySelector("body");
+
+document.addEventListener("scroll", () => {
+  let sectionTop = aboutSection.offsetTop;
+  if (window.scrollY >= sectionTop - 100) {
+    document.querySelector("[href='#about']").classList.add("active");
+    document.querySelector("[href='#hero-section']").classList.remove("active");
+  }
+});
+
+for (;;) {}
+
+// function activeLink() {
+//   let sectionTop = aboutSection.offsetTop;
+//   if (window.scrollY >= sectionTop - 100) {
+//     document.querySelector("[href='#about']").classList.add("active");
+//     document.querySelector("[href='#hero-section']").classList.remove("active");
+//   }
+// }
